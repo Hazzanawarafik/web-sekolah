c3222c6fd0580b51c8c9d59644448b215aa4fedc
hazzanawarafik <hazzanawa@gmail.com>
2023-04-30 19:57:42 +0700

    28 Web Sekolah Dengan Codeigniter - Download Area Part 3

--- application/controllers/Download.php
+++ application/controllers/Download.php
@@ -67,6 +67,82 @@ class Download extends CI_Controller {
         
     }
 
+    public function edit($id_file){
+        $this->form_validation->set_rules('ket_file', 'Keterangan File', 'Required');
+        
+        if ($this->form_validation->run() == TRUE) {
+            $config['upload_path']          = './file/';
+            $config['allowed_types']        = 'doc|docx|ppt|pptx|pdf|txt';
+            $config['max_size']             = 2000;
+
+            $this->upload->initialize($config);
+            if(!$this->upload->do_upload('file'))
+            {
+                $data = array(
+                    'title' => 'SMKN 4 SMG',
+                    'title2' => 'Edit Data File',
+                    'download' => $this->m_download->detail($id_file),
+                    'error_upload' => $this->upload->display_errors(),
+                    'isi' => 'admin/download/v_edit',
+                );
+                $this->load->view('admin/layout/v_wrapper',$data,FALSE);
+            }
+            else
+            {
+                // menghapus file lama
+                $download = $this->m_download->detail($id_file);
+                if($download->file !=""){
+                    unlink('./file/'.$download->file);
+                }
+                // end menghapus file
+
+                $upload_data = array('uploads' => $this->upload->data());
+                $config['image_library'] = 'gd2';
+                $config['source_image'] = './file/'.$upload_data['uploads']['file_name'];
+                $this->load->library('image_lib',$config);
+                
+
+                $data = array(
+                    'id_file' => $id_file,
+                    'ket_file' => $this->input->post('ket_file'),
+                    'file' => $upload_data['uploads']['file_name'],
+                );
+                $this->m_download->edit($data);
+                $this->session->set_flashdata('pesan','Data Berhasil Di Edit !!!');
+                redirect('download');
+            }
+            $data = array(
+                    'id_file' => $id_file,
+                    'ket_file' => $this->input->post('ket_file'),
+                );
+                $this->m_download->edit($data);
+                $this->session->set_flashdata('pesan','Data Berhasil Di Edit !!!');
+                redirect('download');
+        } 
+        $data = array(
+            'title' => 'SMKN 4 SMG',
+            'title2' => 'Edit Data File',
+            'download' => $this->m_download->detail($id_file),
+            'isi' => 'admin/download/v_edit',
+        );
+        $this->load->view('admin/layout/v_wrapper',$data,FALSE);
+        
+    }
+
+    public function delete($id_file){
+        // menghapus file foto lama
+        $download = $this->m_download->detail($id_file);
+        if($download->file !=""){
+            unlink('./file/'.$download->file);
+        }
+        // end menghapus foto
+
+        $data = array('id_file' => $id_file);
+        $this->m_download->delete($data);
+        $this->session->set_flashdata('pesan','Data Berhasil Di Hapus !!!');
+        redirect('download','refresh');
+    }
+
 }
 
 /* End of file Download.php */
